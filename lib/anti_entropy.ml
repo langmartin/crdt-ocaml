34646d43f058a71a80bc49748811f78f09a88349
Lang Martin <lang.martin@gmail.com>
2024-05-31 10:49:42 -0400

    typing

--- lib/anti_entropy.ml
+++ lib/anti_entropy.ml
@@ -1,25 +1,25 @@
 type key = string
-type value = string
-type box = string * value
+type box = string * string
 
 module StringMap = Map.Make(String)
-
-let make_store() : box StringMap.t =
-  StringMap.empty
+let make_store() : box StringMap.t = StringMap.empty
 
 let get_opt store key =
   match StringMap.find_opt store key with
   | None -> None
   | Some (_hulc, value) -> value
 
+let get store key =
+  match get_opt store key with
+  | None -> raise Exit
+  | Some value -> value
+
 let put store key hulc value =
   let open StringMap in
-  let x = find_opt store key in
-  match x with
-  | None -> add store key (hulc, value)
-  | Some box ->
-     let (prev, _) = box in
+  match find_opt key store with
+  | None -> add key (hulc, value) store
+  | Some (prev, _) ->
      if String.compare hulc prev > 0 then
-       add store key (hulc, value)
+       add key (hulc, value) store
      else
        store
