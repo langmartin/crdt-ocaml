e0383fc92504aeadbdb5b5a82121d2529e4aa490
Lang Martin <lang.martin@gmail.com>
2024-06-06 16:16:29 -0400

    schema types down into AE?

--- lib/gossip_capnp.ml
+++ lib/gossip_capnp.ml
@@ -1,6 +1,13 @@
 module Api = Schema.MakeRPC(Capnp_rpc_lwt)
+module Item = Schema.Make(Capnp.BytesMessage)
 
 open Capnp_rpc_lwt
+
+let recv item_msg : string =
+  let open Item.Reader in
+  let key = Item.key_get item_msg in
+  let hulc = Item.hulc_get item_msg in
+  World.ae_put key hulc item_msg
 
 let local =
   let module Gossip = Api.Service.Gossip in
@@ -8,10 +15,11 @@ let local =
     object inherit Gossip.service
       method send_impl params release_param_caps =
         let open Gossip.Send in
-        let _msg = Params.msg_get params in
+        let msg = Params.msg_get params in
         release_param_caps ();
+        let is_fresh = recv msg in
         let response, results = Service.Response.create Results.init_pointer in
-        Results.reply_set results true;
+        Results.reply_set results is_fresh;
         Service.return response
     end
 
@@ -19,8 +27,12 @@ module Gossip = Api.Client.Gossip
 
 open Lwt.Infix
 
-let send t msg =
+let send t key hulc value =
   let open Gossip.Send in
   let request, params = Capability.Request.create Params.init_pointer in
-  Params.msg_set params msg;
+  let rw = Params.msg_init params in
+  let open Item.Builder in
+  Item.key_set rw key;
+  Item.hulc_set rw hulc;
+  Item.value_set rw value;
   Capability.call_for_value_exn t method_id request >|= Results.reply_get
